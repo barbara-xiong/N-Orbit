7bdd97e84099f79ec472133d2cb5ae07f004e26e
barbara-xiong <barbaraxiong17@gmail.com>
2024-09-23 10:18:45 -0400

    fix some variable names and ordering in n-orbit-enrichment

--- NOrbitEnrichment/n-orbit-enrichment.py
+++ NOrbitEnrichment/n-orbit-enrichment.py
@@ -5,7 +5,7 @@ from scipy import stats
 import sys
 
 vectors_path = "/path/to/cluster/vectors/NR_vectors_predicted.csv"
-trials_path = "/path/to/trials/"
+output_path = "/path/to/output/"
 run = sys.argv[1]
 
 # Number of permutations
@@ -15,13 +15,13 @@ num_permutations = 25000
 # Number of cell types (first numCellTypes columns are not permuted)
 numCellTypes = 18
 
-# Subsample size
-sample_size = int(round(len(tcn_vectors_df)*0.2))
-
 # Load data
 tcn_vectors_df = pd.read_csv(vectors_path)
 tcn_vectors_df = tcn_vectors_df.drop(columns="neighborhood")
 tcn_vectors = tcn_vectors_df.values.astype(np.int32)
+
+# Subsample size
+sample_size = int(round(len(tcn_vectors_df)*0.2))
 
 # Perform the enrichment analysis
 p_values = enrichment_analysis(tcn_vectors, num_permutations, numCellTypes, sample_size)
@@ -33,4 +33,4 @@ tcn_vectors_df = tcn_vectors_df.sort_values("pvalue")
 tcn_vectors_df['qvalue'] = stats.false_discovery_control(tcn_vectors_df['pvalue'], method='bh')
 
 # Save the results to a CSV file
-tcn_vectors_df.to_csv(results_path + "NOrbit_Enrichment_Run_"+str(run)+".csv")
+tcn_vectors_df.to_csv(output_path + "NOrbit_Enrichment_Run_"+str(run)+".csv")
